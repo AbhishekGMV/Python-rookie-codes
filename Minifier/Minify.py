c3729250e6f6455f3e9ddaf05f6892bcceeee522
AbhishekGMV <abhi01320@gmail.com>
2020-06-16 16:48:25 +0530

    Added more methods to minify the files optimally

--- Minifier/Minify.py
+++ Minifier/Minify.py
@@ -1,15 +1,33 @@
 import sys
-def minifier():
+import re
+
+def removeComments(file):
+    """
+    Removes all the comments in the file 
+    """
+    with open(file, 'r') as script:
+        mess_file = script.read()
+    clean_file = re.sub(re.compile("/\*.*?\*/"),"" ,mess_file)
+    clean_file = re.sub(re.compile("//.*?\n"),"" ,clean_file) 
+    clean_file = re.sub(re.compile("<!--.*?-->"),"", clean_file)
+    
+    with open(file, 'w') as script:
+        script.write(clean_file)
+
+def minifier(file):
+    """
+    Removes all white spaces in the file 
+    """
     minified =''
-    with open(sys.argv[1],'r') as script_file:
+    with open(file,'r') as script_file:
         for line in script_file.read():
             for char in line.strip():
-                if char == ' ':
-                    continue
-                minified += char    
+                if not char == ' ':
+                    minified += char    
 
-    with open(sys.argv[1],'w') as script_file:
+    with open(file,'w') as script_file:
         script_file.write(minified)            
         
 if __name__ == '__main__':
-    minifier()
+    removeComments(sys.argv[1])
+    minifier(sys.argv[1])
